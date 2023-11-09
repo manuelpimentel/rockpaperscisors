3d52a5c347ba2309b023230a3599d987d620111a
manuel.pimentel <manuel.pimentel@edreamsodigeo.com>
2023-11-09 15:49:14 -0500

    spring init

---
+++ src/main/java/com/manugames/rockpaperscisors/RockpaperscisorsApplication.java
@@ -0,0 +1,13 @@
+package com.manugames.rockpaperscisors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockpaperscisorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RockpaperscisorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manugames/rockpaperscisors/RockpaperscisorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manugames.rockpaperscisors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockpaperscisorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
